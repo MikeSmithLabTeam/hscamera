eda6e8698f38a809bdde994c623c9bf979e4243f
Mike Smith <mikesmithlab@gmail.com>
2022-08-15 14:40:36 +0100

    Update gui.py

--- hscamera/gui.py
+++ hscamera/gui.py
@@ -16,7 +16,7 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.cam = Camera()
+        self.cam = Camera(self)
         self.cam.start()
         self.setup_gui()
 
